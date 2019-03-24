05693f867b0213c0d5a41562680832f453cf3d8e
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-03-24 19:13:29 +0200

    ToString() Override

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -63,5 +63,17 @@ namespace SchoolLibrary
             var result = (a + b + c) / 3;
             return result;
         }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(Name);
+            sb.AppendLine(Address);
+            sb.AppendLine(City);
+            sb.AppendFormat(", {0}  {1}", State, Zip);
+
+            return sb.ToString();
+        }
     }
 }
