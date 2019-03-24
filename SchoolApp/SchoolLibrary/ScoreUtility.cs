7061b195eee73e58c31f377bfd605590a2713e89
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-03-25 01:01:27 +0200

    Interfaces as types

--- SchoolApp/SchoolLibrary/ScoreUtility.cs
+++ SchoolApp/SchoolLibrary/ScoreUtility.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace SchoolLibrary
 {
-    class ScoreUtility
+    public class ScoreUtility
     {
+        public static IScored BestOfTwo(IScored Assignment1, IScored Assignment2)
+        {
+            var score1 = Assignment1.Score / Assignment1.MaximumScore;
+            var score2 = Assignment2.Score / Assignment2.MaximumScore;
+
+            if(score1 > score2)
+            {
+                return Assignment1;
+            }
+            else
+            {
+                return Assignment2;
+            }
+        }
     }
 }
