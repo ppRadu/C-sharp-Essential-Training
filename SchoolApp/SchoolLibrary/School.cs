485169752a63bb68e6b5c75c9529c75bfaae997c
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-03-24 18:41:17 +0200

    Created project and auto-properties
    This is the first commit.
    I have just created the project and added some auto-properties for the School class.

---
+++ SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string PhoneNumber { get; set; }
+    }
+}
